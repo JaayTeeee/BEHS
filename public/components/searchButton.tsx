b5c79f39c828254c38d5b61fdb86776b42588ccb
chongjying <yingchong313@gmail.com>
2024-01-20 01:13:21 +0800

    frontend - searchbox - research

---
+++ public/components/searchButton.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+interface searchButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const searchButton: React.FC<searchButtonProps> = ({ text, onClick }) => {
+  return (
+    <button
+      type="button"
+      style={{
+        color: "white",
+        background: "#339f6b",
+        width: "70px",
+        height: "35px",
+        margin: "10px",
+        border: "none",
+        borderRadius: "8px",
+        marginRight: "-1685px",
+        zIndex: 999,
+      }}
+      onClick={onClick}
+    >
+      <span style={{ fontSize: "15px" }}><b>{text}</b></span>
+    </button>
+  );
+};
+
+export default searchButton;
