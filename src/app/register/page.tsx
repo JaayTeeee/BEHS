e7badcd3b36d77aba5b7ca83d8f6c11622306a3e
JaayTeeee <leejertao@gmail.com>
2024-01-20 12:37:03 +0800

    modified registration page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,3 +1,26 @@
 export default function Registration() {
-  return <main></main>;
+  return (
+    <main>
+      <div id="registration-form">
+        <div className="step-indicator">
+          <ul>
+            <li className="active">
+              <div
+                className="circle"
+                style={{
+                  width: "82px",
+                  height: "82px",
+                  left: "300px",
+                  top: "10px",
+                }}
+              >
+                1
+              </div>
+            </li>
+            <li></li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
 }
