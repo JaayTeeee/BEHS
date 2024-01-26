06009dd2f288581bd1bc95e731b04817b30b25b2
JaayTeeee <leejertao@gmail.com>
2024-01-26 23:51:31 +0800

    Added api for inserting docs

--- src/app/components/LoginButton.tsx
+++ src/app/components/LoginButton.tsx
@@ -1,68 +1,69 @@
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { WalletMultiButton } from "@solana/wallet-adapter-react-ui";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/navigation";
-
 const LoginButton = () => {
   const { connection } = useConnection();
   const { publicKey, sendTransaction, connected } = useWallet();
   const router = useRouter();
+  const WalletMultiButtonDynamic = dynamic(
+    async () =>
+      (await import("@solana/wallet-adapter-react-ui")).WalletMultiButton,
+    { ssr: false }
+  );
 
   if (publicKey != null && connected) {
     console.log("Wallet Address: ", publicKey.toBase58());
     const walletAddress = publicKey.toBase58();
     const checkRequest = new Request("http://localhost:3001/api/checkID", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        mode: "cors",
-        body: JSON.stringify({ walletAddress }),
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      mode: "cors",
+      body: JSON.stringify({ walletAddress }),
     });
 
     fetch(checkRequest)
-        .then((checkResponse) => {
-            if (checkResponse.ok) {
-                // Continue with parsing JSON when the response is OK
-                return checkResponse.json();
-            } else {
-                // Log additional details about the response when it's not OK
-                console.error("Failed to check address. Response details:", checkResponse);
-                // You can return a failed promise here or throw an error if needed
-                throw new Error("Failed to check address");
-            }
-        })
-        .then((checkData) => {
-            if (checkData && checkData.success === true) {
-                const userType = checkData.userType;
-                if (userType === 'user') {
-                    const encodedAddress = encodeURIComponent(walletAddress);
-                    router.push(`/welcome?WalletAddress=${encodedAddress}`);
-                } else if (userType === 'hospital') {
-                    const encodedAddress = encodeURIComponent(walletAddress);
-                    router.push(`/medicalwelcome?WalletAddress=${encodedAddress}`);
-                } else {
-                    console.error('Unknown user type:', userType);
-                }
-            } else {
-                const encodedAddress = encodeURIComponent(walletAddress);
-                router.push(`/register?WalletAddress=${encodedAddress}`);
-            }
-        })
-        .catch((error) => {
-            // Log additional details about the fetch error during check
-            console.error("Fetch error during check:", error);
-            // Handle fetch errors during check
-            throw error;
-        });
-} else {
-    console.log('FAILED');
-}
-
+      .then((checkResponse) => {
+        if (checkResponse.ok) {
+          return checkResponse.json();
+        } else {
+          console.error(
+            "Failed to check address. Response details:",
+            checkResponse
+          );
+          throw new Error("Failed to check address");
+        }
+      })
+      .then((checkData) => {
+        if (checkData && checkData.success === true) {
+          const userType = checkData.userType;
+          if (userType === "user") {
+            const encodedAddress = encodeURIComponent(walletAddress);
+            router.push(`/welcome?WalletAddress=${encodedAddress}`);
+          } else if (userType === "hospital") {
+            const encodedAddress = encodeURIComponent(walletAddress);
+            router.push(`/medicalwelcome?WalletAddress=${encodedAddress}`);
+          } else {
+            console.error("Unknown user type:", userType);
+          }
+        } else {
+          const encodedAddress = encodeURIComponent(walletAddress);
+          router.push(`/register?WalletAddress=${encodedAddress}`);
+        }
+      })
+      .catch((error) => {
+        console.error("Fetch error during check:", error);
+        throw error;
+      });
+  } else {
+    console.log("FAILED");
+  }
 
   return (
     <div>
-      <WalletMultiButton />
+      <WalletMultiButtonDynamic />
     </div>
   );
 };
--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,5 +1,18 @@
 "use client";
 
+import { WalletAdapterNetwork } from "@solana/wallet-adapter-base";
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+import {
+  LedgerWalletAdapter,
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  TorusWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
 import MedRecordImage from "../../../public/icons/icons-medical-record.png";
 import PermissionImage from "../../../public/icons/icons-permission.png";
 import ResearchImage from "../../../public/icons/icons-research.png";
@@ -7,80 +20,98 @@ import ButtonWithImage from "../components/ButtonWithImage";
 import HomePageButton from "../components/HomePageButton";
 import LogoutButton from "../components/LogoutButton";
 import UserProfileButton from "../components/UserProfileButton";
-import CheckUsername from "../components/getUsername";
+import CheckUsername from "../functions/getUsername";
 
 export default function Home() {
+  const solNetwork = WalletAdapterNetwork.Testnet;
+  const endpoint = useMemo(() => clusterApiUrl(solNetwork), [solNetwork]);
+  const wallets = useMemo(
+    () => [
+      new PhantomWalletAdapter(),
+      new SolflareWalletAdapter({ network: solNetwork }),
+      new TorusWalletAdapter(),
+      new LedgerWalletAdapter(),
+    ],
+    [solNetwork]
+  );
+
   return (
-    <main>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "space-between",
-        }}
-      >
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            marginLeft: "20px",
-            marginTop: "20px",
-          }}
-        >
-          <HomePageButton />
-        </div>
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            marginRight: "30px",
-            marginTop: "15px",
-          }}
-        >
-          <UserProfileButton />
-          <LogoutButton />
-        </div>
-      </div>
-      <div
-        style={{
-          textAlign: "center",
-          marginTop: "20px",
-          marginBottom: "100px",
-        }}
-      >
-        <div className="BEHS" style={{ fontSize: "65px" }}>
-          <strong>WELCOME, <CheckUsername /></strong>
-        </div>
-      </div>
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "-20px",
-        }}
-      >
-        <div style={{ marginRight: "20px" }}>
-          <ButtonWithImage
-            text="Research Opportunities"
-            imageSrc={ResearchImage}
-            link="/research"
-          />
-        </div>
-        <div style={{ marginRight: "20px" }}>
-          <ButtonWithImage
-            text="Permissions"
-            imageSrc={PermissionImage}
-            link="/permissions"
-          />
-        </div>
-        <div>
-          <ButtonWithImage
-            text="Medical Records"
-            imageSrc={MedRecordImage}
-            link="/records"
-          />
-        </div>
-      </div>
-    </main>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets}>
+        <main>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginLeft: "20px",
+                marginTop: "20px",
+              }}
+            >
+              <HomePageButton />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginRight: "30px",
+                marginTop: "15px",
+              }}
+            >
+              <UserProfileButton />
+              <LogoutButton />
+            </div>
+          </div>
+          <div
+            style={{
+              textAlign: "center",
+              marginTop: "20px",
+              marginBottom: "100px",
+            }}
+          >
+            <div className="BEHS" style={{ fontSize: "65px" }}>
+              <strong>
+                WELCOME, <CheckUsername />
+              </strong>
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              marginTop: "-20px",
+            }}
+          >
+            <div style={{ marginRight: "20px" }}>
+              <ButtonWithImage
+                text="Research Opportunities"
+                imageSrc={ResearchImage}
+                link="/research"
+              />
+            </div>
+            <div style={{ marginRight: "20px" }}>
+              <ButtonWithImage
+                text="Permissions"
+                imageSrc={PermissionImage}
+                link="/permissions"
+              />
+            </div>
+            <div>
+              <ButtonWithImage
+                text="Medical Records"
+                imageSrc={MedRecordImage}
+                link="/records"
+              />
+            </div>
+          </div>
+        </main>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
