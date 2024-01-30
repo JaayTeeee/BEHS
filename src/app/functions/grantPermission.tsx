a1cade3caed8462c9f248d617aad7c7a6b481d5e
JaayTeeee <leejertao@gmail.com>
2024-01-30 21:58:39 +0800

    remove redirect in grantPermission

--- src/app/functions/grantPermission.tsx
+++ src/app/functions/grantPermission.tsx
@@ -23,8 +23,6 @@ export default function grantPermission(requiredAddress) {
       .then((response) => {
         if (response.success) {
           console.log("Permission granted!");
-          const url = `http://localhost:3000/welcome?WalletAddress=${requiredAddress}`;
-          window.location.href = url;
         } else {
           console.error("Address is null or response is not successful.");
         }
